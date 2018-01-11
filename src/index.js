f944fa201b10f9a8a3e1f2ac6c06103f4b7c1a50
CARLOS\User <carlos.cordoba@media8.com>
2018-01-11 18:34:08 -0300

    - Quitando css del js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import loadImage from 'blueimp-load-image'
 import PropTypes from 'prop-types';
-import css from './style.css'
 
 class AppolodoroUploadImage extends Component {  
 
